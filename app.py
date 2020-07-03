bfd14e7354c805b7aadae40d4940aba9515cab9c
rose <rose@evolution.ai>
2020-07-03 18:59:52 +0100

    add templates folder

--- app.py
+++ app.py
@@ -1,13 +1,21 @@
 #!/usr/bin/env python3
 
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, render_template
 
 
-@app.route('/')
-def index():
-    return 'Welcome to the BBCF website!'
+def create_app():
+    app = Flask(__name__, instance_relative_config=True, static_url_path="")
+
+    app.config.from_pyfile("default.py")
+    app.config.from_envvar("APP_CONFIG_FILE", silent=True)
+
+    @app.route('/')
+    def index():
+        return render_template('index.html')
+
+    return app
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=80, debug=True)
+    flask_app = create_app()
+    flask_app.run(host="0.0.0.0", port=80, debug=True)
