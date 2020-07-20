41cc9a79d6ffe5cf6fbd6c379d92637b86710384
Amarjeet Singh <amarssadal@gmail.com>
2020-07-21 00:13:26 +0100

    final set of updates from me for v1. still a few small bits to do. Rose will have to take over this for now

--- app.py
+++ app.py
@@ -1,13 +1,17 @@
 #!/usr/bin/env python3
 
 from flask import Flask, render_template
-
+from datetime import datetime
 
 def create_app():
     app = Flask(__name__, instance_relative_config=True, static_url_path="")
 
     app.config.from_pyfile("default.py")
     app.config.from_envvar("APP_CONFIG_FILE", silent=True)
+
+    @app.context_processor
+    def inject_now():
+        return {'now': datetime.utcnow()}
 
     @app.route('/')
     @app.route('/index')
